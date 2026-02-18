bff89b20bebe98176f4103acd9e730492bbc5349
Slavina <Slavina@DESKTOP-MQA8F4K>
2026-02-18 12:36:14 +0200

    Add project files.

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
