a98481347c98946932319106c98781a003990716
Slavina <slavina.i.tsoneva@gmail.com>
2026-04-23 15:58:00 +0300

    warning messages added when trying to access detailed pages when not logged in

--- Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -39,6 +39,9 @@ namespace Web.Areas.Identity.Pages.Account
         [BindProperty]
         public InputModel Input { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string AccessMessage { get; set; }
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
